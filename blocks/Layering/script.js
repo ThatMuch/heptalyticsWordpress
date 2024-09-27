5f519fb4942f0eac47ec6e9c352c51bbe386fc85
Mathilde Arconte <signup@marconte.fr>
2024-09-27 12:23:09 +0200

    Refactor: Add GSAP animation library and settings script

---
+++ blocks/Layering/script.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+    gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+
+    const cards = document.querySelectorAll(".pinnedSections__item");
+
+    const animation = gsap.timeline();
+    let cardHeight;
+    function initCards() {
+        animation.clear();
+        cardHeight = cards[0].offsetHeight;
+        cards.forEach((card, index) => {
+            if (index > 0) {
+                gsap.set(card, { y: index * cardHeight });
+                animation.to(card, { y: 0, duration: index * 0.5, ease: "none" }, 0);
+            }
+        });
+    }
+
+    initCards();
+
+    ScrollTrigger.create({
+        trigger: ".pinnedSections__wrapper",
+        start: "top top",
+        pin: true,
+        pinSpacing: true,
+        end: () => `+=${cards.length * cardHeight}`,
+        scrub: true,
+        animation: animation,
+        //markers: true,
+        ease: "none",
+        invalidateOnRefresh: true,
+    });
+
+    ScrollTrigger.addEventListener("refreshInit", initCards);
+});
+
---
+++ blocks/Layering/template.php
@@ -0,0 +1,91 @@
+<?php
+$args = array(
+	'post_type' => 'post',
+	'posts_per_page' => 4,
+
+);
+
+$the_query = new WP_Query($args);
+?>
+
+<div class="container">
+	<!-- <div class="scroll-animation">
+			<div class="scroll__wrapper">
+				<section class="section section_1">
+					ONE
+				</section>
+				<section class="section section_2">
+					TWO
+				</section>
+				<section class="section section_3">
+					THREE
+				</section>
+			</div>
+		</div> -->
+
+	<div class="pinnedSections__wrapper">
+		<div class="pinnedSections">
+			<div class="pinnedSections__item" id="card1">
+				<div>
+					<h4 class="pinnedSections__feature">featured</h4>
+					<h2 class="pinnedSections__title">Scope</h2>
+					<p class="pinnedSections__desc">Completeness of the fight against fraud & AML / CFT (user embedding, business patterns back-testing)</p>
+					<ul class="pinnedSections__list">
+						<li>Hundreds of business patterns are documented</li>
+						<li>Hundreds of business patterns are documented</li>
+						<li>Hundreds of business patterns are documented</li>
+						<li>Hundreds of business patterns are documented</li>
+					</ul>
+				</div>
+				<img src="https://picsum.photos/id/237/200/300" alt="">
+			</div>
+			<div class="pinnedSections__item" id="card1">
+				<div>
+					<h4 class="pinnedSections__feature">featured</h4>
+					<h2 class="pinnedSections__title">Scope</h2>
+					<p class="pinnedSections__desc">Completeness of the fight against fraud & AML / CFT (user embedding, business patterns back-testing)</p>
+					<ul class="pinnedSections__list">
+						<li>Hundreds of business patterns are documented</li>
+						<li>Hundreds of business patterns are documented</li>
+						<li>Hundreds of business patterns are documented</li>
+						<li>Hundreds of business patterns are documented</li>
+					</ul>
+				</div>
+				<img src="https://picsum.photos/id/237/200/300" alt="">
+			</div>
+			<div class="pinnedSections__item" id="card1">
+				<div>
+					<h4 class="pinnedSections__feature">featured</h4>
+					<h2 class="pinnedSections__title">Scope</h2>
+					<p class="pinnedSections__desc">Completeness of the fight against fraud & AML / CFT (user embedding, business patterns back-testing)</p>
+					<ul class="pinnedSections__list">
+						<li>Hundreds of business patterns are documented</li>
+						<li>Hundreds of business patterns are documented</li>
+						<li>Hundreds of business patterns are documented</li>
+						<li>Hundreds of business patterns are documented</li>
+					</ul>
+				</div>
+				<img src="https://picsum.photos/id/237/200/300" alt="">
+			</div>
+		</div>
+	</div>
+
+
+
+
+
+
+	<script src="script.js"> </script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js"
+		integrity="sha512-7eHRwcbYkK4d9g/6tD/mhkf++eoTHwpNM9woBxtPUBWm67zeAfFC+HrdoE2GanKeocly/VxeLvIqwvCdk7qScg=="
+		crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/Observer.min.js"
+		integrity="sha512-pLJlXRr/H5t+k+Tjc+Qe0Z6u6psPak7rvYBdsZ0Z+kG84jn/zifMNTQBZKZlwZC1z23bifGoQWzGnI0eWBJKPQ=="
+		crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollTrigger.min.js"
+		integrity="sha512-onMTRKJBKz8M1TnqqDuGBlowlH0ohFzMXYRNebz+yOcc5TQr/zAKsthzhuv0hiyUKEiQEQXEynnXCvNTOk50dg=="
+		crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollToPlugin.min.js"
+		integrity="sha512-1PKqXBz2ju2JcAerHKL0ldg0PT/1vr3LghYAtc59+9xy8e19QEtaNUyt1gprouyWnpOPqNJjL4gXMRMEpHYyLQ=="
+		crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</div>
